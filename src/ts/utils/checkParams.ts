2019c805bb85f388c3a11d2e2161aa070fded8bf
Evan MASSOL <evan.massol@etu.univ-amu.fr>
2025-01-08 18:11:52 +0100

    Minor change

--- src/ts/utils/checkParams.ts
+++ src/ts/utils/checkParams.ts
@@ -1,4 +1,4 @@
-// Function to validate an ID
+// Function to check if an ID is valid (positive integer)
 export function validateId(id: any, paramName: string): number {
     const parsedId = Number(id);
     if (isNaN(parsedId) || parsedId < 1 || !Number.isInteger(parsedId)) 
